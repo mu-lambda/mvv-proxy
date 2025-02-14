91bc002a0f5d11fbb453122fb6d2f2a2a51324b8
Mu-Lambda <mulambda@mulambda.de>
2025-02-14 15:16:46 +0100

    Error handling

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -12,7 +12,9 @@ type Props = {
 
 type State = 
     { status : "loading" } |
-    { status : "ready", timetable : request.TimetableResponse };
+    { status : "ready", timetable : request.TimetableResponse } |
+    { status : "error", message : string }
+;
 
 function sleep(msec: number): Promise<void> {
     return new Promise((r) => setTimeout(r, msec));
@@ -39,25 +41,37 @@ class DepsTable extends React.Component<Props, State> {
    }
 
    override render(): ReactElement {
-       if (this.state && this.state.status === "ready") {
-           const r = new Renderer(
-               this.#stringCache, new Date(this.state.timetable.date), 
-               this.state.timetable.request);
-           const table = r.renderTable(this.state.timetable.departures);
-           return <div className="box">{table}</div>
-       } else {
-        return <div className="loading-box">
-            <div className="loading">Loading...<span className="loader" /></div>
-        </div>
-       } 
+       switch(this.state.status) {
+           case "ready": {
+               const r = new Renderer(
+                   this.#stringCache, new Date(this.state.timetable.date), 
+                   this.state.timetable.request);
+               const table = r.renderTable(this.state.timetable.departures);
+               return <div className="box">{table}</div>
+           }
+           case "loading": {
+               return <div className="loading-box">
+                   <div className="loading">Loading...<span className="loader" /></div>
+               </div>
+           }
+           case "error": {
+               return <div className="loading-box">
+                   <div className="loading">Error: {this.state.message}</div>
+               </div>
+           }
+       }
    }
 
    async updateLoop() {
        while(!this.#stopUpdating) {
            console.log("Updating");
            const now = Math.floor(new Date().getTime() / 1000);
-           const r = await fetch(`/api/v1/timetable?timestamp=${now}`);
-           this.setState({ status: "ready", timetable: await r.json() }); 
+           try {
+               const r = await fetch(`/api/v1/timetable?timestamp=${now}`);
+               this.setState({ status: "ready", timetable: await r.json() }); 
+           } catch (e) {
+               this.setState({ status : "error", message : `${e}`});
+           }
            if (!this.#update) return;
            await sleep(this.#update);
        }
